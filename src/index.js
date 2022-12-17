547349d3988e44023f4609a20f3e9b45a231395c
sabhinav10 <sabhinav654@gmail.com>
2022-12-17 11:01:11 +0530

    First react component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import React from 'react' //This code is not specific to React. It is general Javascript code.
+import ReactDOM from 'react-dom'
 
+//Stateless Functional componenets
+//Always return JSX
+
+// function Greeting(){
+//   return <h1>This is my first React Component.</h1>
+// }
+
+const Greeting = () =>{
+  return React.createElement('h1',{},"hello world")
+}
+
+ReactDOM.render(<Greeting/>, document.getElementById('root'))
